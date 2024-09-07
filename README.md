e6bb995829e436eea11add3b6c316695963e86bc
Vinayak Chandra Suryavanshi <vinayak.chandra.suryavanshi@gmail.com>
2024-09-07 12:53:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pdf-Reduce-CLI
+
+    Command Line tool to reduce pdf file size.
+
+- Make sure to install `Ghostscript` before running the python program.
+
+`python pdf_file_reduce.py --n /path/to/file.pdf`
---
+++ pdf_file_reduce.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+import pprint
+
+parser = argparse.ArgumentParser(description='Description: Reduce PDF size of written')
+parser.add_argument("--n", type=str, help="Name of the pdf file to be reduced", required=True, )
+
+args = parser.parse_args()
+pprint.pprint(vars(args))
+
+# filePath = str(input("Enter path of file: "))
+# filename = str(input("File name: "))
+filename = args.n
+
+name = filename.replace(".pdf", "")
+
+outputName = f'{name}_reduced.pdf'
+
+setting = "ebook"  # screen, ebook, prepress
+cmd = f'gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/{setting} -dNOPAUSE -dQUIET -dBATCH -sOutputFile=' \
+      f'\"{outputName}\" \"{name}.pdf\"'
+os.system(cmd)  # running the command
+# os.system(f'rm {name}.pdf') # removing the file:
+os.system(f'mv \"{outputName}\" \"{name}\".pdf')  # renaming the file, "mv": is used to move/renaming file
